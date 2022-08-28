70ffe732bc6bc57fbdef1f42439a79a8eb612680
chrismonbiju <109053618+chrismonb@users.noreply.github.com>
2022-08-28 16:04:40 +0530

    Create sutosms.sh

---
+++ sutosms.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+detect_distro() {
+    if [[ "$OSTYPE" == linux-android* ]]; then
+            distro="termux"
+    fi
+
+    if [ -z "$distro" ]; then
+        distro=$(ls /etc | awk 'match($0, "(.+?)[-_](?:release|version)", groups) {if(groups[1] != "os") {print groups[1]}}')
+    fi
+
+    if [ -z "$distro" ]; then
+        if [ -f "/etc/os-release" ]; then
+            distro="$(source /etc/os-release && echo $ID)"
+        elif [ "$OSTYPE" == "darwin" ]; then
+            distro="darwin"
+        else 
+            distro="invalid"
+        fi
+    fi
+}
+
+pause() {
+    read -n1 -r -p "Press any key to continue..." key
+}
